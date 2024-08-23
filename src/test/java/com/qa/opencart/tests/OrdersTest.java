d4b9b2dee6583a8249b2c69abf64afdce8aaa0d3
Sunil Alahari <sunilalahari.automationengineer@gmail.com>
2024-08-23 18:13:43 +0530

    COmmited at 0613

---
+++ src/test/java/com/qa/opencart/tests/OrdersTest.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class OrdersTest {
+	@Test
+	public void Test() {
+		System.out.println("HI I am Feature 2 branch...");
+	}
+
+}
